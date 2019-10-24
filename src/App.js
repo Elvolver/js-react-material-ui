07e26426dba247ed82ff0e7a955df002de21fc9d
elvolver <elvolver@mail.ru>
2019-10-24 15:48:21 +0500

    Initial commit material UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import {makeStyles} from '@material-ui/core/styles';
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Radio from "@material-ui/core/Radio";
+import Button from "@material-ui/core/Button";
+import style from "../src/App.css"
 
 function App() {
+
+  const useStyles = makeStyles(theme => ({
+    appBar: {
+      position: 'relative',
+    },
+    layout: {
+      width: 'auto',
+      marginLeft: theme.spacing(2),
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+        width: 600,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      },
+    },
+    paper: {
+      marginTop: theme.spacing(3),
+      marginBottom: theme.spacing(3),
+      padding: theme.spacing(2),
+      [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+        marginTop: theme.spacing(6),
+        marginBottom: theme.spacing(6),
+        padding: theme.spacing(3),
+      },
+    },
+    stepper: {
+      padding: theme.spacing(3, 0, 5),
+    },
+    buttons: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+    },
+    button: {
+      marginTop: theme.spacing(3),
+      marginLeft: theme.spacing(1),
+    },
+  }));
+
+  const classes = useStyles();
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  const [value, setValue] = React.useState('female');
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className={style.App} >
+        <AppBar position="absolute" color="default" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              Book Appointment
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+          <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+          >
+            <Typography className={classes.heading}>Руки</Typography>
+
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              <FormControl component="fieldset" className={classes.formControl}>
+                <FormLabel component="legend">Услуга</FormLabel>
+                <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+                  <FormControlLabel value="female" control={<Radio color="primary"/>} label="Красить ногти" />
+                  <FormControlLabel value="male" control={<Radio color="primary"/>} label="Пилить ногти" />
+                </RadioGroup>
+              </FormControl>
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <ExpansionPanel expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+          <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2bh-content"
+              id="panel2bh-header"
+          >
+            <Typography className={classes.heading}>Ноги</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              <FormControl component="fieldset" className={classes.formControl}>
+                <FormLabel component="legend">Услуга</FormLabel>
+                <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+                  <FormControlLabel value="female" control={<Radio color="primary"/>} label="Красить ногти" />
+                  <FormControlLabel value="male" control={<Radio color="primary"/>} label="Пилить ногти" />
+                </RadioGroup>
+              </FormControl>
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <Button variant="contained" color="primary" className={classes.button}>
+          Next
+        </Button>
       </header>
     </div>
   );
